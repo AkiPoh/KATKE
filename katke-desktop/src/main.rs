3e7a641e6ef7615384a5df3338256ea5346af32f
AkiPoh <59505647+AkiPoh@users.noreply.github.com>
2025-02-06 17:46:38 +0200

    Render purple window

--- katke-desktop/src/main.rs
+++ katke-desktop/src/main.rs
@@ -1,3 +1,123 @@
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::EventLoop,
+    window::WindowBuilder,
+};
+
+async fn run() {
+    let event_loop = EventLoop::new().unwrap();
+    let window = WindowBuilder::new()
+        .with_title("KATKE")
+        .build(&event_loop)
+        .unwrap();
+
+    let size = window.inner_size();
+    let instance = wgpu::Instance::new(wgpu::InstanceDescriptor::default());
+
+    let surface = instance.create_surface(&window).unwrap();
+    let adapter = instance
+        .request_adapter(&wgpu::RequestAdapterOptions {
+            power_preference: wgpu::PowerPreference::default(),
+            force_fallback_adapter: false,
+            compatible_surface: Some(&surface),
+        })
+        .await
+        .unwrap();
+
+    let (device, queue) = adapter
+        .request_device(
+            &wgpu::DeviceDescriptor {
+                label: None,
+                required_features: wgpu::Features::empty(),
+                required_limits: wgpu::Limits::default(),
+            },
+            None,
+        )
+        .await
+        .unwrap();
+
+    let surface_caps = surface.get_capabilities(&adapter);
+    let surface_format = surface_caps
+        .formats
+        .iter()
+        .copied()
+        .find(|f| f.is_srgb())
+        .unwrap_or(surface_caps.formats[0]);
+
+    let mut config = wgpu::SurfaceConfiguration {
+        usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+        format: surface_format,
+        width: size.width,
+        height: size.height,
+        present_mode: wgpu::PresentMode::Fifo,
+        alpha_mode: surface_caps.alpha_modes[0],
+        view_formats: vec![],
+        desired_maximum_frame_latency: 2,
+    };
+
+    surface.configure(&device, &config);
+
+    let window_ref = &window;
+    event_loop.run(move |event, target| {
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => {
+                target.exit();
+            }
+            Event::WindowEvent {
+                event: WindowEvent::Resized(new_size),
+                ..
+            } => {
+                config.width = new_size.width;
+                config.height = new_size.height;
+                surface.configure(&device, &config);
+            }
+            Event::AboutToWait => {
+                window_ref.request_redraw();
+            }
+            Event::WindowEvent {
+                event: WindowEvent::RedrawRequested,
+                ..
+            } => {
+                let frame = surface.get_current_texture().unwrap();
+                let view = frame.texture.create_view(&wgpu::TextureViewDescriptor::default());
+
+                let mut encoder = device.create_command_encoder(&wgpu::CommandEncoderDescriptor { 
+                    label: Some("Render Encoder")
+                });
+
+                {
+                    let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                        label: Some("Render Pass"),
+                        color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                            view: &view,
+                            resolve_target: None,
+                            ops: wgpu::Operations {
+                                load: wgpu::LoadOp::Clear(wgpu::Color {
+                                    r: 0.4, // Slightly purple background
+                                    g: 0.1,
+                                    b: 0.4,
+                                    a: 1.0,
+                                }),
+                                store: wgpu::StoreOp::Store,
+                            },
+                        })],
+                        depth_stencil_attachment: None,
+                        timestamp_writes: None,
+                        occlusion_query_set: None,
+                    });
+                }
+
+                queue.submit(std::iter::once(encoder.finish()));
+                frame.present();
+            }
+            _ => (),
+        }
+    }).unwrap();
+}
+
 fn main() {
-    println!("Hello, world!");
+    pollster::block_on(run());
 }
